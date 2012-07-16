4a781484feecbfb7d4d78088c93d5bc63e31c88a
Marcelo Emanoel <marceloemanoel@gmail.com>
2012-07-16 14:13:45 -0300

    using Directions constants rather than Navigator's

--- spec/navigator_spec.rb
+++ spec/navigator_spec.rb
@@ -3,17 +3,17 @@ require "spec_helper"
 describe Navigator do
   context "standing at position (0,0) facing NORTH" do
     before :each do
-      @navigator = Navigator.new(Navigator::NORTH)
+      @navigator = Navigator.new(Directions::NORTH)
     end
 
     it "turns left and faces WEST" do
       @navigator.turn_left
-      @navigator.direction.should == Navigator::WEST
+      @navigator.direction.should == Directions::WEST
     end
 
     it "turns right and faces EAST" do
       @navigator.turn_right
-      @navigator.direction.should == Navigator::EAST
+      @navigator.direction.should == Directions::EAST
     end
 
     it "moves and stands at position (0, 1)" do
@@ -24,17 +24,17 @@ describe Navigator do
 
   context "standing at position (0, 0) facing WEST" do
     before :each do
-      @navigator = Navigator.new(Navigator::WEST)
+      @navigator = Navigator.new(Directions::WEST)
     end
 
     it "turns left and faces SOUTH" do
       @navigator.turn_left
-      @navigator.direction.should == Navigator::SOUTH
+      @navigator.direction.should == Directions::SOUTH
     end
 
     it "turns right and faces NORTH" do
       @navigator.turn_right
-      @navigator.direction.should == Navigator::NORTH
+      @navigator.direction.should == Directions::NORTH
     end
 
     it "moves and stands at position (-1, 0)" do
@@ -46,17 +46,17 @@ describe Navigator do
 
   context "standing at position (0,0) facing SOUTH" do
     before :each  do
-      @navigator = Navigator.new(Navigator::SOUTH)
+      @navigator = Navigator.new(Directions::SOUTH)
     end
 
     it "turns left and faces EAST" do
       @navigator.turn_left
-      @navigator.direction.should == Navigator::EAST
+      @navigator.direction.should == Directions::EAST
     end
 
     it "turns right and faces WEST" do
       @navigator.turn_right
-      @navigator.direction.should == Navigator::WEST
+      @navigator.direction.should == Directions::WEST
     end
 
     it "moves and stands at position (0, -1)" do
@@ -67,17 +67,17 @@ describe Navigator do
 
   context "standing at position (0, 0) facing EAST" do
     before :each do
-      @navigator = Navigator.new(Navigator::EAST)
+      @navigator = Navigator.new(Directions::EAST)
     end
 
     it "turns left and faces NORTH" do
       @navigator.turn_left
-      @navigator.direction.should == Navigator::NORTH
+      @navigator.direction.should == Directions::NORTH
     end
 
     it "turns right and faces SOUTH" do
       @navigator.turn_right
-      @navigator.direction.should == Navigator::SOUTH
+      @navigator.direction.should == Directions::SOUTH
     end
 
     it "moves and stands at position (1, 0)" do
