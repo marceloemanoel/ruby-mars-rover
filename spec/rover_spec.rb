cf6ff16fd70b5f005c458a0241259b377f23095b
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2012-07-17 21:45:16 -0300

    refactoring the rover spec. Reducing the duplication of code.

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,109 +1,105 @@
 require "spec_helper"
 
-describe Rover do
-  describe "#turn_left" do
+describe Rover do  
+  context "standing at position (0, 0)" do
     context "facing NORTH" do
-      it "faces WEST" do
-        @rover = Rover.new(Directions::NORTH)
-        @rover.turn_left
-        @rover.direction.should == Directions::WEST
+      let(:rover) {Rover.new (Directions::NORTH)}
+    
+      describe "#turn_left" do    
+        it "faces WEST" do
+          rover.turn_left
+          rover.direction.should == Directions::WEST
+        end
       end
-    end
-
-    context "facing WEST" do
-      it "faces SOUTH" do
-        @rover = Rover.new(Directions::WEST)
-        @rover.turn_left
-        @rover.direction.should == Directions::SOUTH
-      end
-    end
-
-    context "facing SOUTH" do
-      it "faces EAST" do
-        @rover = Rover.new(Directions::SOUTH)
-        @rover.turn_left
-        @rover.direction.should == Directions::EAST
-      end
-    end
-
-    context "facing EAST" do
-      it "faces NORTH" do
-        @rover = Rover.new(Directions::EAST)
-        @rover.turn_left
-        @rover.direction.should == Directions::NORTH
-      end
-    end
-  end
-
-  describe "#turn_right" do
-    context "facing NORTH" do
-      it "faces EAST" do
-        @rover = Rover.new(Directions::NORTH)
-        @rover.turn_right
-        @rover.direction.should == Directions::EAST
-      end
-    end
-
-    context "facing EAST" do
-      it "faces SOUTH" do
-        @rover = Rover.new(Directions::EAST)
-        @rover.turn_right
-        @rover.direction.should == Directions::SOUTH
-      end
-    end
-
-    context "facing SOUTH" do
-      it "faces WEST" do
-        @rover = Rover.new(Directions::SOUTH)
-        @rover.turn_right
-        @rover.direction.should == Directions::WEST
-      end
-    end
-
-    context "facing WEST" do
-      it "faces NORTH" do
-        @rover = Rover.new(Directions::WEST)
-        @rover.turn_right
-        @rover.direction.should == Directions::NORTH
-      end
-    end
-  end
-
-  describe "#move" do
-    context "standing at position (0, 0)" do
-
-      context "facing NORTH" do
+    
+      describe "#turn_right" do
+        it "faces EAST" do
+          rover.turn_right
+          rover.direction.should == Directions::EAST
+        end      
+      end                 
+    
+      describe "#move" do
         it "stands at position (0, 1)" do
-          @rover = Rover.new(Directions::NORTH)
-          @rover.move
-          @rover.position.should == [0, 1]
+          rover.move
+          rover.position.should == [0, 1]
         end
       end
-
-      context "facing WEST" do
+    end
+  
+    context "facing WEST" do
+      let (:rover) {Rover.new(Directions::WEST)}
+   
+      describe "#turn_left" do
+        it "faces SOUTH" do
+          rover.turn_left
+          rover.direction.should == Directions::SOUTH
+        end
+      end
+    
+      describe "#turn_right" do  
+        it "faces NORTH" do
+          rover.turn_right
+          rover.direction.should == Directions::NORTH
+        end
+      end
+    
+      describe "#move" do   
         it "stands at position (-1, 0)" do
-          @rover = Rover.new(Directions::WEST)
-          @rover.move
-          @rover.position.should == [-1, 0]
+          rover.move
+          rover.position.should == [-1, 0]
         end
       end
-
-      context "facing SOUTH" do
+    end  
+  
+    context "facing SOUTH" do
+      let(:rover) {Rover.new(Directions::SOUTH)}
+    
+      describe "#turn_left" do
+        it "faces EAST" do
+          rover.turn_left
+          rover.direction.should == Directions::EAST
+        end
+      end
+    
+      describe "#turn_right" do
+        it "faces WEST" do
+          rover.turn_right
+          rover.direction.should == Directions::WEST
+        end
+      end
+    
+      describe "#move" do
         it "stands at position (0, -1)" do
-          @rover = Rover.new(Directions::SOUTH)
-          @rover.move
-          @rover.position.should == [0, -1]
+          rover.move
+          rover.position.should == [0, -1]
+        end
+      end
+    end
+  
+    context "facing EAST" do
+      let(:rover) {Rover.new(Directions::EAST)}
+    
+      describe "#turn_left" do
+        it "faces NORTH" do
+          rover.turn_left
+          rover.direction.should == Directions::NORTH
         end
       end
 
-      context "facing EAST" do
+      describe "#turn_right" do
+        it "faces SOUTH" do
+          rover.turn_right
+          rover.direction.should == Directions::SOUTH
+        end
+      end
+    
+      describe "#move" do
         it "stands at position (1, 0)" do
-          @rover = Rover.new(Directions::EAST)
-          @rover.move
-          @rover.position.should == [1, 0]
+          rover.move
+          rover.position.should == [1, 0]
         end
       end
-
     end
   end
 end
