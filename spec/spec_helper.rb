ae0aa6a29aea4d521ef1a98bc6ba857a03358bd0
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2012-07-17 21:46:23 -0300

    configuring rspec mocking framework

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
-require_relative "../src/communicator"
+require_relative "../src/operator"
 require_relative "../src/directions"
 require_relative "../src/rover"
+
+RSpec.configure do |config|
+  config.mock_framework = :rspec
+end
